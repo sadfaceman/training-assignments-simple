0132e82c5cfc3da2e4896c77daeb3ffafca4a8d0
Leif Jakobsen <noticon@live.dk>
2020-06-18 10:20:50 +0200

    Removed shadow fields

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -2,8 +2,6 @@ package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
 public class CheckingAccount extends Account {
-    private static final float INTEREST_PERCENTAGE = 0.01f;
-    private Money balance = new Money();
     private int transferLimit = 100;
 
     public Transfer makeTransfer(String counterAccount, Money amount)
